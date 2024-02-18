4b0b977a2782a71d58c989f69aefe2284c47fa67
hakar90 <159024977+hakar90@users.noreply.github.com>
2024-02-18 22:09:58 +0200

    Update Assignment1StaticCode.c

--- Assignment1StaticCode.c
+++ Assignment1StaticCode.c
@@ -37,7 +37,6 @@ void save_pgm(const char *filename, int image[HEIGHT][WIDTH]) {
     fprintf(pgmimg, "P2\n"); // Writing Magic Number to the File
     fprintf(pgmimg, "%d %d\n", WIDTH, HEIGHT); // Writing Width and Height
     fprintf(pgmimg, "255\n"); // Writing the maximum gray value
-    int count = 0;
 
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
@@ -49,88 +48,47 @@ void save_pgm(const char *filename, int image[HEIGHT][WIDTH]) {
     fclose(pgmimg);
 }
 
-void master(int size, int s, int image[HEIGHT][WIDTH]) {
-    struct complex c;
-
-    for (int i = 1, curr = s; i < size; i++, curr = curr + s) {
-        MPI_Send(&curr, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-    }
-
-    int i, j;
-    for (i = 0; i < s; i++) {
-        for (j = 0; j < WIDTH; j++) {
-            c.real = (j - WIDTH / 2.0) * 4.0 / WIDTH;
-            c.imag = (i - HEIGHT / 2.0) * 4.0 / HEIGHT;
-            image[i][j] = cal_pixel(c);
-        }
-    }
-    int curr;
-    for (int i = 1; i < size; i++) {
-        MPI_Recv(&curr, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (int row = 0; row < s; row++) {
-            MPI_Recv(&image[row + curr], WIDTH, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-    }
-
-    save_pgm("static.pgm", image);
-}
-
-void slave(int s, int image[HEIGHT][WIDTH]) {
-    int curr;
-    struct complex c;
-    int i, j;
-    MPI_Recv(&curr, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-    for (i = curr; i < curr + s; i++) {
-        for (j = 0; j < WIDTH; j++) {
-            c.real = (j - WIDTH / 2.0) * 4.0 / WIDTH;
-            c.imag = (i - HEIGHT / 2.0) * 4.0 / HEIGHT;
-            image[i][j] = cal_pixel(c);
-        }
-    }
-
-    MPI_Send(&curr, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    for (int row = 0; row < s; row++) {
-        MPI_Send(&image[row + curr], WIDTH, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
-        MPI_Barrier(MPI_COMM_WORLD); // Synchronize processes
-
-}
-
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     int rank;
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     int s = HEIGHT / size;
-
+    struct complex c;
     int image[HEIGHT][WIDTH];
+    int local[s][WIDTH];
     double AVG = 0;
     int N = 10; // number of trials
     double total_time[N];
-    
-    for (int k=0; k<N; k++){
-      clock_t start_time = clock(); // Start measuring time
-
+    for (int k = 0; k < N; k++) {
+    clock_t start_time;
     if (rank == 0) {
-        master(size, s, image);
-	MPI_Barrier(MPI_COMM_WORLD); // Synchronize processes
-
-        clock_t end_time = clock(); // End measuring time
-
-      total_time[k] = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-      printf("Execution time of trial [%d]: %f seconds\n", k , total_time[k]);
-      AVG += total_time[k];
-    } else {
-        slave(s, image);
+        start_time = clock(); // Start measuring time
+        }
+    	for (int i = rank * s; i < (rank + 1) * s; i++) {
+        	for (int j = 0; j < WIDTH; j++) {
+            		c.real = (j - WIDTH / 2.0) * 4.0 / WIDTH;
+            		c.imag = (i - HEIGHT / 2.0) * 4.0 / HEIGHT;
+            		local[i - rank * s][j] = cal_pixel(c);
+        }
     }
-    
-    
-    }
-          if(rank==0){printf("The average execution time of 10 trials is: %f ms", AVG/N*1000);
+        MPI_Barrier(MPI_COMM_WORLD); // Synchronize processes
+
+    	if(rank==0){
+    	clock_t end_time = clock(); // End measuring time
+
+        total_time[k] = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+        printf("Execution time of trial [%d]: %f seconds\n", k, total_time[k]);
+        AVG += total_time[k];
 }
+    	MPI_Gather(local, s * WIDTH, MPI_INT, image, s * WIDTH, MPI_INT, 0, MPI_COMM_WORLD);
+    	}
+    if (rank == 0) {
+            printf("The average execution time of 10 trials is: %f ms\n", AVG / N * 1000);
+        save_pgm("static.pgm", image);
+    }
+
     MPI_Finalize();
     return 0;
 }
